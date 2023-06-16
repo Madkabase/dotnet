1101f4710ebb527e9f3ef0ef712bae3992287382
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-06-16 18:29:39 +0200

    code cleaning

--- IoDit.WebAPI/Utilities/Repositories/CompanyFarmUserRepository.cs
+++ IoDit.WebAPI/Utilities/Repositories/CompanyFarmUserRepository.cs
@@ -14,14 +14,22 @@ class CompanyFarmUserRepository : ICompanyFarmUserRepository
         DbContext = context;
     }
 
+    /// <summary>
+    /// get the users of the company
+    /// </summary>
+    /// <param name="companyId"> the id of the company </param>
+    /// <param name="companyFarmId"> the id of the company farm </param>
+    /// <returns>The users of the company</returns>
     public async Task<IQueryable<CompanyFarmUser>> GetUsersOfCompanyByCompanyId(long companyId) =>
         await Task.Run(() =>
             DbContext.Companies.Include(x => x.FarmUsers).Where(x => x.Id == companyId).SelectMany(x => x.FarmUsers));
 
-    public async Task<IQueryable<CompanyFarmUser>> GetCompanyUserFarmUsers(long companyUserId) =>
-        await Task.Run(() =>
-            DbContext.CompanyFarmUsers.Where(x => x.CompanyUserId == companyUserId));
-
+    /// <summary>
+    /// get the company with the given id if it includes the isuer
+    /// </summary>
+    /// <param name="companyId"> the id of the company </param>
+    /// <param name="userId"> the id of the user </param>
+    /// <returns>the company with the given id if it includes the isuer</returns>
     public async Task<CompanyFarmUser?> GetCompanyUserFarmUser(long companyFarmId, long companyUserId) =>
         await Task.Run(() =>
             DbContext.CompanyFarmUsers.FirstOrDefault(x =>
--- IoDit.WebAPI/Utilities/Repositories/interface/ICompanyFarmUserRepository.cs
+++ IoDit.WebAPI/Utilities/Repositories/interface/ICompanyFarmUserRepository.cs
@@ -5,6 +5,5 @@ namespace IoDit.WebAPI.Utilities.Repositories;
 public interface ICompanyFarmUserRepository
 {
     Task<IQueryable<CompanyFarmUser>> GetUsersOfCompanyByCompanyId(long companyId);
-    Task<IQueryable<CompanyFarmUser>> GetCompanyUserFarmUsers(long companyUserId);
     Task<CompanyFarmUser?> GetCompanyUserFarmUser(long companyFarmId, long companyUserId);
 }
