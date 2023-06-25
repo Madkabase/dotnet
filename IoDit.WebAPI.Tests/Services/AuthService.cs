b78bbfdf6f32728a2e53989cae029857ce4fa347
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-06-25 13:46:47 +0200

    add test project

---
+++ IoDit.WebAPI.Tests/Services/AuthService.cs
@@ -0,0 +1,17 @@
+using IoDit.WebAPI.Persistence.Entities;
+using IoDit.WebAPI.Persistence.Repositories;
+using IoDit.WebAPI.Services;
+using IoDit.WebAPI.Utilities.Helpers;
+using Moq;
+
+namespace IoDit.WebAPI.Tests.Services;
+
+public class AuthServiceTest
+{
+
+    [Fact]
+    public void ShouldLogin()
+    {
+
+    }
+}
