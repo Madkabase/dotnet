014deb93953df6355bc1dbcc38d80f5d55ba1ac1
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-07-14 17:27:02 +0200

    device creation dtos

--- IoDit.WebAPI/DTO/Device/CreateDeviceRequestDto.cs
+++ IoDit.WebAPI/DTO/Device/CreateDeviceRequestDto.cs
@@ -5,4 +5,6 @@ public class CreateDeviceRequestDto
     public long FieldId { get; set; }
     public string Name { get; set; }
     public string DevEUI { get; set; }
+    public string JoinEUI { get; set; }
+    public string AppKey { get; set; }
 }
