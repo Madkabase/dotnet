dc8952930c5c6b0a289108974e4cb10ff4b27e83
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-07-19 12:13:03 +0200

    add main sensor choice on threshold

---
+++ IoDit.WebAPI/Migrations/20230719074437_main_sensor_choice_for_threshold.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IoDit.WebAPI.Migrations
+{
+    public partial class main_sensor_choice_for_threshold : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MainSensor",
+                table: "Thresholds",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MainSensor",
+                table: "Thresholds");
+        }
+    }
+}
---
+++ IoDit.WebAPI/Utilities/Types/MainSensor.cs
@@ -0,0 +1,8 @@
+namespace IoDit.WebAPI.Utilities.Types
+{
+    public enum MainSensor
+    {
+        SensorUp,
+        SensorDown
+    }
+}
