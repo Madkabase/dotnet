f1285ec6dd26f35f2a7cdc84029fab6ed6f618d9
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-08-03 10:39:24 +0200

    add foreign field id to model device

--- IoDit.WebAPI/Migrations/20230803083848_foreign_ids_in_model_device.Designer.cs
+++ IoDit.WebAPI/Migrations/20230803083848_foreign_ids_in_model_device.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IoDit.WebAPI.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IoDit.WebAPI.Migrations
 {
     [DbContext(typeof(AgroditDbContext))]
-    partial class IoDitDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230803083848_foreign_ids_in_model_device")]
+    partial class foreign_ids_in_model_device
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
