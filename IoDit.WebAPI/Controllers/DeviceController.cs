5a00e42ba982b322edc26a0eec4d30d4778597af
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-08-04 09:56:15 +0200

    add check on user can change a field

--- IoDit.WebAPI/Controllers/DeviceController.cs
+++ IoDit.WebAPI/Controllers/DeviceController.cs
@@ -1,9 +1,9 @@
 using IoDit.WebAPI.BO;
 using IoDit.WebAPI.DTO.Device;
-using IoDit.WebAPI.Persistence.Entities;
 using IoDit.WebAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using IoDit.WebAPI.Config.Exceptions;
+using System.Security.Claims;
 
 namespace IoDit.WebAPI.Controllers;
 
@@ -13,16 +13,29 @@ public class DeviceController : ControllerBase, IBaseController
 {
 
     private readonly IDeviceService _deviceService;
+    private readonly IUserService _userService;
+    private readonly IFieldService _fieldService;
 
-    public DeviceController(IDeviceService deviceService)
+    public DeviceController(
+        IDeviceService deviceService,
+        IUserService userService,
+        IFieldService fieldService
+    )
     {
         _deviceService = deviceService;
+        _userService = userService;
+        _fieldService = fieldService;
     }
 
     [HttpPost]
     public async Task<ActionResult<DeviceDto>> CreateDevice([FromBody] CreateDeviceRequestDto createDeviceRequestDto)
     {
         // TODO: Check if the user has rigths to create a device
+        bool canChangeField = await _fieldService.UserCanChangeField(createDeviceRequestDto.FieldId, await GetRequestDetails());
+        if (!canChangeField)
+        {
+            throw new UnauthorizedAccessException("User does not have rights to change this field");
+        }
         try
         {
             var getDevice = await _deviceService.GetDeviceByDevEUI(createDeviceRequestDto.DevEUI);
@@ -42,12 +55,17 @@ public class DeviceController : ControllerBase, IBaseController
             return Ok(DeviceDto.FromBo(device));
         }
         catch (BadHttpRequestException) { throw; }
-        catch (System.Exception) { throw; }
+        catch (Exception) { throw; }
     }
 
     [ApiExplorerSettings(IgnoreApi = true)]
-    public Task<UserBo> GetRequestDetails()
+    public async Task<UserBo> GetRequestDetails()
     {
+        var claimsIdentity = User.Identity as ClaimsIdentity;
+        var userIdClaim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+        var userId = (userIdClaim?.Value) ?? throw new UnauthorizedAccessException("Invalid user");
+        var user = await _userService.GetUserByEmail(userId);
+        return user;
         throw new NotImplementedException();
     }
 }
