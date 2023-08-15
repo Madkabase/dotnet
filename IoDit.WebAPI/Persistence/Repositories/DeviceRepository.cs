ece61290fb950519040c926250969a6a5cd94f11
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-08-15 14:40:21 +0200

    add last data to device when get By Id

--- IoDit.WebAPI/Persistence/Repositories/DeviceRepository.cs
+++ IoDit.WebAPI/Persistence/Repositories/DeviceRepository.cs
@@ -15,7 +15,19 @@ namespace IoDit.WebAPI.Persistence.Repositories
 
         public async Task<Device?> GetDeviceByDevEUI(string devEUI)
         {
-            return await _dbContext.Devices.FirstOrDefaultAsync(d => d.DevEUI == devEUI);
+
+            var device = await _dbContext.Devices
+                .FirstOrDefaultAsync(d => d.DevEUI == devEUI);
+            if (device == null)
+            {
+                return null;
+            }
+            device.DeviceDatas = _dbContext.DeviceData
+            .Where(dd => dd.TimeStamp.ToLocalTime() > DateTime.Now.ToLocalTime().AddDays(-1))
+            .Where(dd => dd.DevEUI == device.DevEUI)
+            .OrderByDescending(dd => dd.TimeStamp)
+            .ToList();
+            return device;
         }
 
         public async Task<Device> CreateDevice(FieldBo fieldBo, DeviceBo device)
