0743dd8f8a15d2d939211ed651c1d74d7c33d3a4
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-08-17 13:10:39 +0200

    74 - add Alerts to dthe database and create Bo and Dto object

---
+++ IoDit.WebAPI/Migrations/20230817093135_alerts_on_fields.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IoDit.WebAPI.Migrations
+{
+    public partial class alerts_on_fields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alerts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AlertType = table.Column<int>(type: "integer", nullable: false),
+                    FieldId = table.Column<long>(type: "bigint", nullable: true),
+                    Closed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alerts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alerts_Fields_FieldId",
+                        column: x => x.FieldId,
+                        principalTable: "Fields",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_FieldId",
+                table: "Alerts",
+                column: "FieldId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alerts");
+        }
+    }
+}
