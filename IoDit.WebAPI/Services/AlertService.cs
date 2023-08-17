75b82439d783e3a5e0bbeb608803a553c56a210f
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-08-17 14:55:14 +0200

    74 - create service to maange business code for alerts

---
+++ IoDit.WebAPI/Services/AlertService.cs
@@ -0,0 +1,40 @@
+using IoDit.WebAPI.BO;
+using IoDit.WebAPI.Persistence.Entities;
+using IoDit.WebAPI.Persistence.Repositories;
+
+namespace IoDit.WebAPI.Services;
+
+public class AlertService : IAlertService
+{
+    private readonly IAlertRepository _alertRepository;
+
+    public AlertService(IAlertRepository alertRepository)
+    {
+        _alertRepository = alertRepository;
+    }
+
+    public async Task<AlertBo> CreateAlert(AlertBo alert)
+    {
+        Alert newAlert = await _alertRepository.CreateAlert(alert);
+        alert.Id = newAlert.Id;
+        return alert;
+    }
+
+    public Task<List<AlertBo>> GetAlertsByField(FieldBo field)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> hasActiveAlert(FieldBo field)
+    {
+        await CloseOutDatedAlerts(field);
+        List<Alert> alerts = await _alertRepository.GetActiveAlertsByField(field);
+        return !(alerts.Count == 0);
+    }
+
+    public async Task CloseOutDatedAlerts(FieldBo fieldbo)
+    {
+        await _alertRepository.CloseOutDatedAlerts(fieldbo);
+    }
+
+}
