c54356be71d603a29721e2d7a979930802668cb5
Pierre Leroyer <pierre.leroyer69@gmail.com>
2023-09-01 16:58:32 +0200

    91 - add delete device service

--- IoDit.WebAPI/Services/Interfaces/IDeviceService.cs
+++ IoDit.WebAPI/Services/Interfaces/IDeviceService.cs
@@ -5,6 +5,7 @@ namespace IoDit.WebAPI.Services;
 public interface IDeviceService
 {
     public Task<DeviceBo> CreateDevice(FieldBo fieldBo, DeviceBo deviceBo);
+    Task DeleteDevice(string devEUI, FieldBo fieldBo);
     Task DeleteDevicesFromField(long fieldId);
     Task<DeviceBo> GetDeviceByDevEUI(string devEUI);
 }
