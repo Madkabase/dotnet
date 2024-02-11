7413f3e8073def6ca160b6eb71539095be0e5633
Pierre Leroyer <pierre.leroyer69@gmail.com>
2024-02-11 18:58:36 +0100

    #126 - create migration fo nullable Battery LEvel

---
+++ IoDit.WebAPI/Migrations/20240211174856_nullableBatteryData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IoDit.WebAPI.Migrations
+{
+    public partial class nullableBatteryData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BatteryLevel",
+                table: "DeviceData",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BatteryLevel",
+                table: "DeviceData",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
